79c928369095eae2f02b8014cd2090243db78ccb
Zeynebesra <zeynep.ozturk@mobilion.com.tr>
2022-01-05 21:58:54 +0300

    Search eklentisi

--- article/views.py
+++ article/views.py
@@ -6,6 +6,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def articles(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword:
+        articles = Article.objects.filter(title__contains = keyword)
+        return render(request,"articles.html",{"articles":articles})
+        
     articles = Article.objects.all()
 
     return render(request,"articles.html",{"articles":articles})
